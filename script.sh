1cf8ccf8408aec63edc8334393d08cde1aed7a0e
rangoDJ <jiten.dutia@gmail.com>
2018-09-09 20:53:41 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 algoMode=$1
 poolUrl=$2
 poolUser=$3
@@ -8,6 +8,6 @@ useScheduler=$6 # true / false
 startTime=$7 #e.g. 1530 or 1100 for time
 stopTime=$8 #e.g. 1530 or 1100 for time
 days=$9 #e.g. "Tuesday,Friday"
-miner="/usr/local/bin/cpuminer"
+miner="/cpuminer/cpuminer"
 
 $miner -a x16r -o stratum+tcp://ravenminer.com:3336 -u RFG2SL3jtheTPg6c9GKUNzKmQQg6uNfegk -p c=RVN
